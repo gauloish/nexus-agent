81ba4878e12d63e3851fbe2e66897dc7ae83faa9
gauloish <125079293+gauloish@users.noreply.github.com>
2026-06-06 16:35:37 -0300

    feat: integrate ingestion process in nexus agent

--- src/agents/agent.py
+++ src/agents/agent.py
@@ -2,16 +2,16 @@ from pathlib import Path
 
 from agno.agent import Agent
 from agno.models.google import Gemini
+from agno.knowledge.embedder.google import GeminiEmbedder
+from agno.knowledge.knowledge import Knowledge
+from agno.vectordb.qdrant import Qdrant
 
 from google.oauth2 import service_account
 
-PROJECT_ROOT = Path(__file__).resolve().parents[2]
-credentials_path = PROJECT_ROOT / "credentials.json"
+from src.knowledge.ingestion import ingest
 
-credentials = service_account.Credentials.from_service_account_file(
-    filename=credentials_path,
-    scopes=["https://www.googleapis.com/auth/cloud-platform"]
-)
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+COLLECTION_NAME = "nexus-collection"
 
 
 def agent() -> Agent:
@@ -21,15 +21,39 @@ def agent() -> Agent:
         Agent: Nexus agent
     """
 
+    credentials_path = PROJECT_ROOT / "credentials.json"
+    files_path = PROJECT_ROOT / "resource/files.json"
+
+    credentials = service_account.Credentials.from_service_account_file(
+        filename=credentials_path,
+        scopes=["https://www.googleapis.com/auth/cloud-platform"]
+    )
+
     model = Gemini(
         id="gemini-2.5-flash",
         vertexai=True,
-        project_id="infraestrutura-enap",
-        location="us-central1",
         credentials=credentials,
     )
+
+    embedder = GeminiEmbedder(
+        id="gemini-embedding-001",
+        vertexai=True,
+    )
+
+    vector_db = Qdrant(
+        collection=COLLECTION_NAME,
+        url="http://localhost:6333",
+        embedder=embedder,
+    )
+
+    knowledge = Knowledge(
+        vector_db=vector_db,
+    )
+
+    ingest(knowledge, files_path)
 
     return Agent(
         name="Nexus Agent",
         model=model,
+        knowledge=knowledge,
     )
