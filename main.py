facf093e8d92b2552d7cd4999530c806635551c8
gauloish <125079293+gauloish@users.noreply.github.com>
2026-06-06 16:37:53 -0300

    fix: load environment variables in main code

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from agno.os import AgentOS
 
 from src.agents import agent
+from dotenv import load_dotenv
+
+load_dotenv()
 
 agent_os = AgentOS(agents=[agent()])
 app = agent_os.get_app()
@@ -9,5 +12,5 @@ app = agent_os.get_app()
 if __name__ == "__main__":
     agent_os.serve(
         app="main:app",
-        reload=True,
+        # reload=True,
     )
