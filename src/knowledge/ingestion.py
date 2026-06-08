5fc3e09f5eca3285d6549b7c8b063c417d94c182
gauloish <125079293+gauloish@users.noreply.github.com>
2026-06-08 14:28:37 -0300

    docs: add some function documentation

--- src/knowledge/ingestion.py
+++ src/knowledge/ingestion.py
@@ -4,6 +4,14 @@ from agno.knowledge.reader.pdf_reader import PDFReader
 from agno.knowledge.chunking.document import DocumentChunking
 
 def extract_files(files_path):
+    """Extract from JSON content with files metadata to list of dictionary
+
+    Args:
+        files_path (str): Path to JSON file
+
+    Returns:
+        list: List of dictionary with files metadata (title and url)
+    """
     data = []
 
     with open(files_path, "r", encoding="utf-8") as file:
@@ -13,6 +21,13 @@ def extract_files(files_path):
 
 
 def ingest(knowledge, files_path):
+    """Ingestion step of knowledge agent. The process consists in download, parsing and chunking
+    of the files with metadata (title and url) extract from `files_path`.
+
+    Args:
+        knowledge (Knoledge): Agent knowledge
+        files_path (str): Path of JSON file with metadata of the files
+    """
     files = extract_files(files_path)
 
     reader = PDFReader(
